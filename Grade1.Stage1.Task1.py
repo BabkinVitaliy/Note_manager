37addb31f02f890f228ea8824ac8650af895ea58
BabkinVitaliy <babkin-vi@mail.ru>
2025-01-27 20:11:44 +0300

    greetings.py

---
+++ Grade1.Stage1.Task1.py
@@ -0,0 +1,21 @@
+# 1 username
+first, second = 'username', 'Vitaliy'
+print( first, second, sep=' : ')
+# 2 title
+first, second = 'Grade 1', 'title'
+print( second, first, sep=' : ')
+# 3 content
+first, second = 'content', 'Stage 1, этап 1'
+print( first, second, sep=' : ')
+# 4 status
+first, second = 'Task 1', 'status'
+print(second, first, sep=' : ')
+# 5 created_date
+first, second = 'creator_date', ' 20.01.2025'
+print( first,second,sep=' : ')
+# 6 issue_date
+first, second ='issue_date', '23.01.2005'
+print( first, second, sep=' : ')
+
+
+
