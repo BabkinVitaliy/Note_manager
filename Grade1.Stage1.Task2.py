390928be69705db355a8586cf937e844cbf939bb
BabkinVitaliy <babkin-vi@mail.ru>
2025-01-27 20:14:51 +0300

    date_changer.py

---
+++ Grade1.Stage1.Task2.py
@@ -0,0 +1,11 @@
+first, second = "creator_date" , "19.03.2024"
+print( first, second, sep=' : ')
+first, second = "insue_date" , "19 march 2024"
+print( first, second, sep=' : ')
+
+first, second = "creator_date" , "19.03.2024"
+info = "19.03.2024"
+print(first, info[0:5],sep=" : " )
+first, second = "insue_date" , "19 march 2024"
+info = "19 march 2024"
+print(first, info[0:-5],sep=" : " )
